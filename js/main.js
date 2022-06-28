dd6409732a01eeee2c2ffc26be40a02ff398b14a
Константин Селютин <kselyutin@nobilis.team>
2022-06-28 14:17:16 +0300

    add modal load

--- js/main.js
+++ js/main.js
@@ -1,37 +1,6 @@
-import {
-  updateMiniatures
-} from './miniature.js';
-
+import {updateMiniatures} from './miniature.js';
+import { onLoadPhoto } from './loadPhotoModal.js';
+import { validateData } from './validateData.js';
 updateMiniatures();
-
-const inputFile = document.querySelector('#upload-file');
-const form = document.querySelector('.img-upload__form');
-inputFile.addEventListener('change', onAddPictureClick);
-
-function onAddPictureClick(event) {
-  document.querySelector('.img-upload__overlay .img-upload__preview img').src = URL.createObjectURL(event.target.files[0]);
-  document.querySelector('.img-upload__overlay').classList.remove('hidden');
-  document.body.classList.add('modal-open');
-
-}
-
-
-const pristine = new Pristine(form);
-const inputHashTag = form.querySelector('.text__hashtags');
-pristine.addValidator(inputHashTag, validateHashtag, 'Формат хэш тега неверный');
-form.addEventListener('submit', onFileSubmit);
-
-function onFileSubmit(evt) {
-  evt.preventDefault();
-  pristine.validate();
-}
-
-function validateHashtag(value) {
-  const regex = new RegExp(/^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/);
-  if(regex.test(value)) {
-    return true;
-  }
-  else{
-    return false;
-  }
-}
+onLoadPhoto();
+validateData();
